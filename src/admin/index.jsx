aad9ba8209ee5c8809ab72be5316b6d13f6a019a
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2021-07-25 08:00:24 +0100

    To manage admin components.

---
+++ src/admin/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Admin () {
+
+    return (
+        <div className="APP">
+            <ul className="sidebar">
+                <li>Dashboard</li>
+                <li>List</li>
+                <li>Contact</li>
+                <li>Adress</li>
+                <li>About</li>
+            </ul>
+        </div>
+    );
+
+}
