fc1dbea22bc8ef2bedb1b7bd6e3b37f7b5b19f5f
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2021-07-25 08:03:49 +0100

    To manage user components.

---
+++ src/user/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function User () {
+
+    return (
+        <div className="APP">
+            <h1>This is the user page !</h1>
+        </div>
+    );
+
+}
